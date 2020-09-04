244e26246cbaabfe42182e3892682a5811a54911
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 07:43:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,10 +10,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min
          min = value
-         min.push(keymin)
     end
   end
-  keymin[0]
+  min
 end 
 binding.pry
 
