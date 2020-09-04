646583f5dd44d3f5878826f293f9bcb6c528f784
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 07:25:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,14 +7,13 @@ def key_for_min_value(name_hash)
   key0, val0 = name_hash.first
   val0 = min
   minhash = []
-  name_hash.each do |key, value|
-    if value < min
-      min = value
-      key = keymin
-    end
-  end
+   name_hash.each do |key, value|
+     if value < min
+       min = value
+       key = keymin
+     end
+   end
   minhash.push(keymin)
-  minhash[0]
 end
 binding.pry
 
