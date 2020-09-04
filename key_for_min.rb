35145a5f13080ac44263d74df85fa4bae6a59420
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 07:40:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,17 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450, :desk => 32}
 
 
 def key_for_min_value(name_hash)
+  keymin = []
   key0, val0 = name_hash.first
   min = val0.to_f
   min
   name_hash.each do |key, value|
     if value < min
          min = value
-         keymin = key
+         keymin.push(key)
     end
   end
-  keymin.to_s
+  keymin[0]
 end 
 binding.pry
 
