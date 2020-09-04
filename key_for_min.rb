fb8e621da96170aa92fa94efbf60e832ad5c5ead
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 06:57:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
+require 'pry'
 
+name_hash = {:chair => 25, :table => 85, :mattress => 450, :desk => 32}
 
 
 def key_for_min_value(name_hash)
-
+  min = ""
+  name_hash.each do |key, value|
+    
+  end
 end
+binding.pry
+
+
+
+
