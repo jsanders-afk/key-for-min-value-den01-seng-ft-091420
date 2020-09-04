039e90f02297ed8362a3911dbf8ea3c784260dd1
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 07:26:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,6 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450, :desk => 32}
 def key_for_min_value(name_hash)
   key0, val0 = name_hash.first
   val0 = min
-  binding.pry
    name_hash.each do |key, value|
      if value < min
        min = value
